63ffae84903fb4384be4fcb8e5d7432c75c3d245
ujaann <ujanst@gmail.com>
2025-07-02 17:49:31 +0545

    new_event screen before state notifier

--- lib/features/event/new_event_screen.dart
+++ lib/features/event/new_event_screen.dart
@@ -1,8 +1,10 @@
 import 'package:calendar_view/calendar_view.dart';
 import 'package:daily/entity/event.dart';
 import 'package:daily/features/event/data/event_repo.dart';
+import 'package:daily/theme/theme_common.dart';
 import 'package:daily/util/snackbars.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 
@@ -55,13 +57,16 @@ class NewEventScreen extends ConsumerWidget {
         actions: [
           IconButton(
               onPressed: () {
-                ref.watch(eventRepoProvider).addEvent(EventEntity(
-                    userId: "Ujan",
-                    eventData: CalendarEventData(
-                        title: "title",
-                        date: DateTime.now(),
-                        startTime: DateTime.now(),
-                        endTime: DateTime.now().add(Duration(hours: 1)))));
+                ref.watch(eventRepoProvider).addEvent(
+                      EventEntity(
+                          userId: "Ujan",
+                          title: "title",
+                          date: DateTime.now(),
+                          startTime: TimeOfDay(hour: 1, minute: 10),
+                          endTime: TimeOfDay(hour: 2, minute: 10),
+                          description: '',
+                          recurrenceSettings: null),
+                    );
               },
               icon: Icon(Icons.done))
         ],
@@ -97,10 +102,15 @@ class NewEventScreen extends ConsumerWidget {
                     await showDatePicker(
                       context: context,
                       initialDate: DateTime.now(),
-                      firstDate: DateTime(2022),
-                      lastDate: DateTime(2030),
+                      firstDate: DateTime(2024),
+                      lastDate: DateTime(2050),
                     ).then((selectedDate) {
-                      if (selectedDate != null) {
+                      if (selectedDate != null && context.mounted) {
+                        if (selectedDate.isBefore(ref.read(_endDateProvider)) &&
+                            !ref.read(_selectedOccurenceOrEndDate)) {
+                          showErrorSnackbar(context, "End date");
+                          return;
+                        }
                         ref.watch(_dateProvider.notifier).state = selectedDate;
                       }
                     });
@@ -112,7 +122,7 @@ class NewEventScreen extends ConsumerWidget {
                         onPressed: () async {
                           await showTimePicker(
                                   context: context,
-                                  initialTime: TimeOfDay.now())
+                                  initialTime: ref.read(_startTimeProvider))
                               .then(
                             (selectedTime) {
                               if (selectedTime != null) {
@@ -124,7 +134,7 @@ class NewEventScreen extends ConsumerWidget {
                                   return;
                                 }
 
-                                ref.read(_endTimeProvider.notifier).state =
+                                ref.read(_startTimeProvider.notifier).state =
                                     selectedTime;
                               }
                             },
@@ -136,7 +146,7 @@ class NewEventScreen extends ConsumerWidget {
                         onPressed: () async {
                           await showTimePicker(
                                   context: context,
-                                  initialTime: TimeOfDay.now())
+                                  initialTime: ref.read(_endTimeProvider))
                               .then(
                             (selectedTime) {
                               if (selectedTime != null) {
@@ -164,10 +174,23 @@ class NewEventScreen extends ConsumerWidget {
               spacing: 12,
               children: [
                 Icon(Icons.repeat),
-                Flexible(
-                    child: TextField(
-                  decoration: InputDecoration(hintText: "Add title"),
-                ))
+                DropdownMenu<RepeatFrequency>(
+                    initialSelection: RepeatFrequency.daily,
+                    helperText: "Repeat",
+                    dropdownMenuEntries: [
+                      DropdownMenuEntry(
+                          value: RepeatFrequency.doNotRepeat,
+                          label: "Do Not repeat"),
+                      DropdownMenuEntry(
+                          value: RepeatFrequency.daily, label: "Daily"),
+                      DropdownMenuEntry(
+                          value: RepeatFrequency.monthly, label: "Monthly"),
+                      DropdownMenuEntry(
+                          value: RepeatFrequency.weekly, label: "Weekly"),
+                      DropdownMenuEntry(
+                          value: RepeatFrequency.yearly, label: "Yearly"),
+                    ]),
+                Toggle()
               ],
             ),
             Row(
@@ -177,6 +200,7 @@ class NewEventScreen extends ConsumerWidget {
                 Flexible(
                     child: TextField(
                   minLines: 3,
+                  maxLines: 5,
                   decoration: InputDecoration(hintText: "Description"),
                   controller: descriptionController,
                 ))
@@ -185,6 +209,78 @@ class NewEventScreen extends ConsumerWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+final _endDateProvider = StateProvider.autoDispose<DateTime>((ref) {
+  return DateTime.now();
+});
+
+final _occurenceControllerProvider =
+    Provider.autoDispose<TextEditingController>((ref) {
+  final controller = TextEditingController();
+  ref.onDispose(() => controller.dispose());
+  return controller;
+});
+
+final _selectedOccurenceOrEndDate = StateProvider<bool>((ref) {
+  return true;
+});
+
+class Toggle extends ConsumerWidget {
+  const Toggle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Radio(
+              value: true,
+              groupValue: ref.watch(_selectedOccurenceOrEndDate),
+              onChanged: (_) {
+                ref.read(_selectedOccurenceOrEndDate.notifier).state = true;
+              },
+            ),
+            SizedBox(
+                width: 100,
+                height: 40,
+                child: TextField(
+                  enabled: ref.watch(_selectedOccurenceOrEndDate),
+                  controller: ref.watch(_occurenceControllerProvider),
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  cursorHeight: 20,
+                  decoration: const InputDecoration(
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 2, horizontal: 6)),
+                )),
+          ],
+        ),
+        Row(
+          children: [
+            Radio(
+              value: false,
+              groupValue: ref.watch(_selectedOccurenceOrEndDate),
+              onChanged: (_) {
+                ref.read(_selectedOccurenceOrEndDate.notifier).state = false;
+              },
+            ),
+            SizedBox(
+                width: 100,
+                child: ElevatedButton(
+                    onPressed:
+                        ref.watch(_selectedOccurenceOrEndDate) ? null : () {},
+                    child: Text(
+                      "End Date",
+                      style: FontsDaily.subText,
+                    )))
+          ],
+        ),
+      ],
     );
   }
 }
